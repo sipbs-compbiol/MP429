8c04fdc7d642c4883fc61f4028f0a402c62c84c8
Leighton Pritchard <leighton.pritchard@mac.com>
2021-02-05 15:04:33 +0000

    add Rscript for dependency installation

---
+++ install.R
@@ -0,0 +1,3 @@
+install.packages("DT")         # for interactive data tables in standalone HTML
+install.packages("plotly")     # for interactive graphs in standalone HTML
+install.packages("tidyverse")  # for convenient data processing
