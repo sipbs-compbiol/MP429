937f97fc2827be2b58d9fc66c5c49641d8f65501
Leighton Pritchard <leighton.pritchard@hutton.ac.uk>
2021-02-06 19:17:50 +0000

    add notebook on exploring an interesting dataset
    This required collecting the GvHD dataset, updating the install
    script to add GGally, ggpubr and ggplot2 packages.

--- install.R
+++ install.R
@@ -1,3 +1,6 @@
 install.packages("DT")         # for interactive data tables in standalone HTML
+install.packages("GGally")     # for graphical representations that ggplot doesn't do
+install.packages("ggplot2")    # for pretty data visualisation, and example datasets
+install.packages("ggpubr")     # for ggplot2 multigraph layouts
 install.packages("plotly")     # for interactive graphs in standalone HTML
 install.packages("tidyverse")  # for convenient data processing
