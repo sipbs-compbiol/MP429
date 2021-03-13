1263229d1425efc779f6cc102d3300fbbf4046d4
Leighton Pritchard <leighton.pritchard@mac.com>
2021-03-13 17:50:41 +0000

    add ggridges to install.R script

--- install.R
+++ install.R
@@ -3,5 +3,6 @@ install.packages("DT")         # for interactive data tables in standalone HTML
 install.packages("GGally")     # for graphical representations that ggplot doesn't do
 install.packages("ggplot2")    # for pretty data visualisation, and example datasets
 install.packages("ggpubr")     # for ggplot2 multigraph layouts
+install.packages("ggridges")   # for ggplot2 joyplot/ridgeplot/stacked density plots
 install.packages("plotly")     # for interactive graphs in standalone HTML
 install.packages("tidyverse")  # for convenient data processing
