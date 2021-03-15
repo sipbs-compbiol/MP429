b38e7699ef6975a6f158c55180412d82797f74dd
Leighton Pritchard <leighton.pritchard@mac.com>
2021-03-15 18:11:56 +0000

    update install script to add latex2exp fror LaTeX in ggplot

--- install.R
+++ install.R
@@ -4,5 +4,6 @@ install.packages("GGally")     # for graphical representations that ggplot doesn
 install.packages("ggplot2")    # for pretty data visualisation, and example datasets
 install.packages("ggpubr")     # for ggplot2 multigraph layouts
 install.packages("ggridges")   # for ggplot2 joyplot/ridgeplot/stacked density plots
+install.packages("latex2exp")  # for expressing LaTeX in ggplot 
 install.packages("plotly")     # for interactive graphs in standalone HTML
 install.packages("tidyverse")  # for convenient data processing
